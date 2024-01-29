5b9da489dcb21cf73556489dcf058d0d47dbb148
Norvh <norah.wadani@gmail.com>
2024-01-29 23:20:09 +0300

    Initial Commit

---
+++ subog/subogApp.swift
@@ -0,0 +1,17 @@
+//
+//  subogApp.swift
+//  subog
+//
+//  Created by Norah W on 17/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct subogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
